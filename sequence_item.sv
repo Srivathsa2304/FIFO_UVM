316c60b2ff3d927721dfc7f31fdf9ae6e2c87778
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-11 15:48:35 +0530

    Update sequence_item.sv

--- sequence_item.sv
+++ sequence_item.sv
@@ -1,6 +1,8 @@
 //SEQUENCE ITEM
+//`include "uvm_macros.svh"
+// import uvm_pkg::*;
 
-class fifo_sequence_items extends sequence_item;
+class fifo_sequence_items extends uvm_sequence_item;
 rand bit i_wren;
 rand bit i_rden;
 rand bit [127:0] i_wrdata;
@@ -9,7 +11,7 @@ bit o_alm_full;
 bit o_empty;
 bit o_alm_empty;
 bit [127:0] o_rddata;
-`uvm_object_utils_begin(fifo_sequence_items)
+  `uvm_object_utils_begin(fifo_sequence_items)
   `uvm_field_int(i_wren, UVM_ALL_ON)
   `uvm_field_int(i_rden, UVM_ALL_ON)
   `uvm_field_int(i_wrdata, UVM_ALL_ON)
@@ -20,7 +22,7 @@ bit [127:0] o_rddata;
   `uvm_field_int(o_rddata, UVM_ALL_ON)
   `uvm_object_utils_end
 
-  constraint c1{{i_wren,i_rden} inside {00,01,10,11};}
+  //constraint c1{{i_wren,i_rden} inside {00,01,10,11};}
   
   // function void pre_randomize();
   //   if(i_rden)
