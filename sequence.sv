d9046f61b3c30484333949bef7e7a6a38b31a9fd
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-11 16:03:20 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -1,1 +1,12 @@
-class fifo_sequence extends uvm_sequence #();
+class fifo_sequence extends uvm_sequence #(fifo_sequence_items);
+  `uvm_object_utils(fifo_sequence)
+  
+  function new(string name = "fifo_sequence");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+    `uvm_info(get_type_name(), $sformatf("** Generate 1024 Write transactions**"), UVM_LOW)
+    repeat(1024)
+      
+    
