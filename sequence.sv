3fa9604c2e14138c07db40115c53e97019b4ed97
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-11 16:16:56 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -8,6 +8,12 @@ class fifo_sequence extends uvm_sequence #(fifo_sequence_items);
   virtual task body();
     `uvm_info(get_type_name(), $sformatf("** Generate 1024 Write transactions**"), UVM_LOW)
     repeat(1024)
+      begin
+         req = fifo_sequence_items::type_id::create("req");
+        start_item(req);
+        assert(req.randomize() with {i_wren == 1;i_rden == 0;});
+        finish_item(req);
+      end
       
       
     
