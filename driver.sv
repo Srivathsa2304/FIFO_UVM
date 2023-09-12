0719d7b2715f5605c16c834e6b8639180694a810
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-12 12:53:47 +0530

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -1,2 +1,10 @@
 class fifo_driver extends uvm_driver #(fifo_sequence_items);
+  virtual fifo_interface vif;
+  fifo_sequence_items req;
+  `uvm_component_utils(fifo_driver)
+
+  function new(string name="fifo_driver", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
   
