3fadc0722197b320cd4091b0674300ac8b0e0093
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-12 10:17:42 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -24,5 +24,14 @@ class fifo_sequence extends uvm_sequence #(fifo_sequence_items);
         assert(req.randomize() with {i_wren == 0;i_rden == 1;});
         finish_item(req);
       end
+
+    `uvm_info(get_type_name(), $sformatf("**Parallel write and read**"), UVM_LOW)
+    repeat(1024)
+      begin
+         req = fifo_sequence_items::type_id::create("req");
+        start_item(req);
+        assert(req.randomize() with {i_wren == 1;i_rden == 1;});
+        finish_item(req);
+      end
       
     
