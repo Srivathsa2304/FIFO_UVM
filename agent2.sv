5cada4f4a05263a360b05dd42f1b81afb418da75
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-13 11:45:42 +0530

    Update agent2.sv

--- agent2.sv
+++ agent2.sv
@@ -1,1 +1,22 @@
+class fifo_agent2 extends uvm_agent;
+  fifo_monitor2 f_mon2;
+  `uvm_component_utils(fifo_agent2)
 
+  function new(string name = "fifo_agent2", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(get_is_active() == UVM_PASSIVE) begin
+      f_mon2 = fifo_monitor2::type_id::create("f_mon2", this);
+    end
+  endfunction
+  
+  // virtual function void connect_phase(uvm_phase phase);
+  //   if(get_is_active() == UVM_PASSIVE)
+  //     f_dri.seq_item_port.connect(f_seqr.seq_item_export);
+  // endfunction
+  
+endclass
