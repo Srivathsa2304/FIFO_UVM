44dd5fda2be581121c67bc2fba61385ce94c816d
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-14 15:20:33 +0530

    Update new.sv

--- new.sv
+++ new.sv
@@ -14,39 +14,81 @@ class fifo_scoreboard extends uvm_scoreboard;
   endfunction
 
   int queue[$];
+  bit [127:0] local_var;
 
-  function void write(input fifo_sequence_items item_got1);
+  function void write1(input fifo_sequence_items item_got1);
     bit [127:0] data;
     if(item_got1.i_wren=='b1)
       begin
         if(queue.size()<1024)
           begin
             queue.push_back(item_got1.i_wren);
-            // if(queue.size()>1020 && queue.size()<1024)
-            //   begin
-            //     $display("Almost full condition: o_alm_full=%0b",item_got2.o_alm_full);
-            //   end
             `uvm_info("Data write operation", $sformatf("i_wren: %0b i_rden: %0b i_wrdata: %0d",item_got1.i_wren, item_got1.i_rden,item_got1.i_wrdata), UVM_LOW);
           end
       end
-
-    
 
     if (item_got1.i_rden == 'b1)
       begin
       if(queue.size() >= 'd1)
         begin
          data = queue.pop_front();
-          if(queue.size()>0 && queue.size()<2)
-            begin
-              $display("Almost empty condition : o_alm_empty=%0b",item_got2.o_alm_empty);
-            end
-        `uvm_info("Data read operation", $sformatf("data: %0d o_rddata: %0d o_empty: %0b o_alm_empty: %0b", data, item_got2.o_rddata, item_got2.o_empty item_got2.o_alm_empty), UVM_LOW);
-          if(data == item_got2.o_rddata)
+        `uvm_info("Data read operation", $sformatf("data: %0d", data), UVM_LOW);
+          if(data == local_var)
         $display("-----------Pass!-----------");
           else
         $display("-----------Fail!-----------");
         end
+      end
+  endfunction
+
+  function void write2(input fifo_sequence_items item_got2);
+local_var=item_got2.o_rddata;
+    if(queue.size()==1024)
+      begin
+        if(item_got2.o_full=='b1)
+          begin
+            $display("FIFO FULL:PASS");
+          end
+        else
+          begin
+            $display("FIFO FULL:FAIL");
+          end
+      end
+
+    if(queue.size()>1020 && queue.size()<1024)
+      begin
+        if(item_got2.o_alm_full=='b1)
+          begin
+            $display("FIFO ALMOST FULL: PASS");
+          end
+        else
+          begin
+            $display("FIFO ALMOST FULL:FAIL");
+          end
+      end
+
+    if(queue.size()>0 && queue.size()<=2)
+      begin
+        if(item_got2.o_alm_empty=='b1)
+          begin
+            $display("FIFO ALMOST EMPTY: PASS");
+          end
+        else
+          begin
+            $display("FIFO ALMOST EMPTY:FAIL");
+          end
+      end
+
+    if(queue.size()==0)
+      begin
+        if(item_got2.o_empty=='b1)
+          begin
+            $display("FIFO EMPTY: PASS");
+          end
+        else
+          begin
+            $display("FIFO EMPTY:FAIL");
+          end
       end
   endfunction
 endclass
