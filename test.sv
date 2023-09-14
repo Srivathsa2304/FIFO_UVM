a6bacdb046d2001ffe6b5d19ab6d0ffd75e1f560
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-14 10:16:35 +0530

    Update test.sv

--- test.sv
+++ test.sv
@@ -1,23 +1,23 @@
-`include "f_environment.sv"
+//`include "f_environment.sv"
 
-class f_test extends uvm_test;
-  f_sequence f_seq;
-  f_environment f_env;
-  `uvm_component_utils(f_test)
+class fifo_test extends uvm_test;
+  fifo_sequence f_seq;
+  fifo_environment f_env;
+  `uvm_component_utils(fifo_test)
   
-  function new(string name = "wrrd_test", uvm_component parent);
+  function new(string name = "fifo_test", uvm_component parent);
     super.new(name, parent);
   endfunction
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    f_seq = f_sequence::type_id::create("f_seq", this);
-    f_env = f_environment::type_id::create("f_env", this);
+    f_seq = fifo_sequence::type_id::create("f_seq", this);
+    f_env = fifo_environment::type_id::create("f_env", this);
   endfunction
   
   virtual task run_phase(uvm_phase phase);
     phase.raise_objection(this);
-    f_seq.start(f_env.f_agt.f_seqr);
+    f_seq.start(f_env.f_agt1.f_seqr);
     phase.drop_objection(this);
     phase.phase_done.set_drain_time(this, 100);
   endtask
